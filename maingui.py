f3b18af501e33c643e6845e335b8d5b80023cb05
amariszhangg <115908439+amariszhangg@users.noreply.github.com>
2023-11-04 16:57:03 +0800

    Update maingui.py

--- maingui.py
+++ maingui.py
@@ -10,26 +10,32 @@ cursor.execute("SELECT student_id, name, login_time FROM Student WHERE student_i
 student_info = cursor.fetchall()
 student_id = student_info[0][0]
 
+# show courses taken
 cursor.execute("SELECT course_code FROM Enrolled WHERE student_id='%s'" % student_id)
 courses = cursor.fetchall()
 print(courses)
 
+# show each course's info
 cursor.execute("SELECT * FROM Course WHERE course_code='%s'" % courses[0][0])
 course_info = cursor.fetchall()
 print(course_info)
 
-cursor.execute("SELECT * FROM Schedule WHERE course_code='%s' ORDER BY start_time" % courses[0][0])
+#show schedule
+cursor.execute("SELECT * FROM Schedule NATURAL JOIN (SELECT course_code FROM Enrolled WHERE student_id='%s') as courses ORDER BY start_time" % student_id)
 schedule = cursor.fetchall()
 print(schedule)
 
+# show each course's material sections
 cursor.execute("SELECT * FROM CourseMaterialSection WHERE course_code='%s'" % courses[0][0])
 course_sections = cursor.fetchall()
 print(course_sections)
 
+# show each course material
 cursor.execute("SELECT * FROM Material WHERE course_code='%s'" % courses[0][0])
 course_materials = cursor.fetchall()
 print(course_materials)
 
+# show each course's latest message
 cursor.execute("SELECT * FROM TeacherMessage WHERE course_code='%s' ORDER BY post_time DESC LIMIT 1" % courses[0][0])
 latest_message = cursor.fetchall()
 print(latest_message)
