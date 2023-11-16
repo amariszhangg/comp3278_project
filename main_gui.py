c50e9ee4455079c2d4bc4e84473aec6321d67de1
EddieED <aolaeddie@gmail.com>
2023-11-17 03:54:26 +0800

    Add schedule and change nav bar to horizontal

--- main_gui.py
+++ main_gui.py
@@ -10,7 +10,7 @@ from PyQt5.QtCore import Qt
 
 main_widget = QWidget()
 
-nav_layout = QVBoxLayout()
+nav_layout = QHBoxLayout()
 
 navbar = QWidget()
 navbar.setLayout(nav_layout)
@@ -23,10 +23,9 @@ button_stylesheet = '''
             QToolButton {
                 background-color: #4AA080;
                 color: white;
-                border: 0;
-                border-radius: 5px;
+                border-radius: 3px;
                 width: 100%;
-                padding: 10px;
+                padding: 5px;
             }
 
             QToolButton:hover {
@@ -36,32 +35,24 @@ button_stylesheet = '''
 
 
 home_button = QToolButton()
-home_button.setText("Home")
 home_button.setIcon(QIcon('assets/home.png'))
 home_button.setIconSize(QSize(35, 35))
 home_button.setStyleSheet(button_stylesheet)
-home_button.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
 
 timetable_button = QToolButton()
-timetable_button.setText("Timetable")
 timetable_button.setIcon(QIcon('assets/calendar.png'))
 timetable_button.setIconSize(QSize(35, 35))
 timetable_button.setStyleSheet(button_stylesheet)
-timetable_button.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
 
 course_button = QToolButton()
-course_button.setText("Course")
 course_button.setIcon(QIcon('assets/course.png'))
 course_button.setIconSize(QSize(35, 35))
 course_button.setStyleSheet(button_stylesheet)
-course_button.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
 
 logout_button = QToolButton()
-logout_button.setText("Logout")
 logout_button.setIcon(QIcon('assets/logout.png'))
 logout_button.setIconSize(QSize(35, 35))
 logout_button.setStyleSheet(button_stylesheet)
-logout_button.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
 main_widget.logout_button = logout_button
 
 nav_layout.addWidget(home_button)
@@ -89,10 +80,10 @@ home_button.clicked.connect(switch_tab_generator(0))
 timetable_button.clicked.connect(switch_tab_generator(1))
 course_button.clicked.connect(switch_tab_generator(2))
 
-main_layout = QHBoxLayout()
-main_layout.addWidget(navbar)
+main_layout = QVBoxLayout()
 main_layout.addWidget(stack)
-main_layout.setStretch(0, 1)
-main_layout.setStretch(1, 9)
+main_layout.addWidget(navbar)
+main_layout.setStretch(0, 8)
+main_layout.setStretch(8, 9)
 
 main_widget.setLayout(main_layout)
--- schedule.py
+++ schedule.py
@@ -1,18 +1,56 @@
 from PyQt5.QtWidgets import QApplication, QFrame, QWidget, QMainWindow, QMessageBox, QLabel
-from PyQt5.QtWidgets import QStackedWidget
+from PyQt5.QtWidgets import QStackedWidget, QTableWidget
 from PyQt5.QtWidgets import QWidget, QStackedWidget
 from PyQt5.QtWidgets import QToolButton
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QTableWidgetItem
 from PyQt5.QtCore import Qt, QSize
 
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QColor
 from PyQt5.QtCore import Qt
+import math
+import database
 
 schedule_widget = QWidget()
 
 schedule_layout = QVBoxLayout()
 schedule_layout.setAlignment(Qt.AlignCenter)
 
-schedule_layout.addWidget(QLabel("This is schedule page"))
+schedule = QTableWidget()
+schedule.setColumnCount(8)
+schedule.setRowCount(28)
+schedule.verticalHeader().hide()
+schedule.setHorizontalHeaderLabels(("", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
+
+for i in range(14):
+    schedule.setSpan(i * 2, 0, 2, 1)
+    time = f"0{i+8}:00"
+    if (i >= 2):
+        time = f"{i+8}:00"
+    time_item = QTableWidgetItem(time)
+    time_item.setFlags(Qt.ItemIsEnabled)
+    schedule.setItem(i * 2, 0, time_item)
+
+student_schedule = database.getSchedule(1)
+
+for student_course in student_schedule:
+    course_code = student_course[0]
+    start_date_time = student_course[1]
+    end_date_time = student_course[2]
+    venue = student_course[3]
+
+    column = start_date_time.isoweekday()
+
+    row = math.ceil((start_date_time.hour - 8) * 60 / 30) + 1
+    rowSpan = math.ceil((end_date_time - start_date_time).seconds / 60 / 30)
+
+    schedule.setSpan(row, column, rowSpan, 1)
+    time_item = QTableWidgetItem(f"{course_code}\n{venue}")
+    time_item.setFlags(Qt.ItemIsEnabled)
+    time_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+    time_item.setBackground(QColor("#4AA080"))
+    schedule.setItem(row, column, time_item)
+    
+schedule_layout.addWidget(schedule)
+
 
 schedule_widget.setLayout(schedule_layout)
