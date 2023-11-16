f121bb04849c9a15a39b6133af27d4ad2f511677
Jonathan <114774547+KunJoNaTHan@users.noreply.github.com>
2023-11-17 05:08:42 +0800

    Update main.py
    fixed the face.py cannot run problem, now can successfully input student_id -> do face recognition -> enter into the main_gui

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import sys
 import database
-
 from PyQt5.QtWidgets import QApplication, QFrame, QWidget, QMainWindow, QMessageBox
 from PyQt5.QtWidgets import QStackedWidget, QHBoxLayout, QLabel
-
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
-
 import tkinter.messagebox as MessageBox
 from tkinter import *
-
 import urllib
 import numpy as np
 import mysql.connector
@@ -34,100 +30,6 @@ main_stack.addWidget(main_widget)
 
 load_dotenv()
 
-def recognize_faces():
-    # Create database connection
-    myconn = mysql.connector.connect(host="localhost",
-        user=os.environ["MYSQL_USER"],
-        passwd=os.environ["MYSQL_PASSWORD"],
-        database=os.environ["MYSQL_DATABASE"])
-    date = datetime.utcnow()
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    cursor = myconn.cursor()
-
-    # Load recognize and read label from model
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.read("FaceRecognition/train.yml")
-
-    labels = {"person_name": 1}
-    with open("FaceRecognition/labels.pickle", "rb") as f:
-        labels = pickle.load(f)
-        labels = {v: k for k, v in labels.items()}
-
-    # Create text-to-speech engine
-    engine = pyttsx3.init()
-    rate = engine.getProperty("rate")
-    engine.setProperty("rate", 175)
-
-    # Define camera and detect face
-    face_cascade = cv2.CascadeClassifier('FaceRecognition/haarcascade/haarcascade_frontalface_default.xml')
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-
-        for (x, y, w, h) in faces:
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = frame[y:y + h, x:x + w]
-            id_, conf = recognizer.predict(roi_gray)
-
-            if conf >= 30:
-                font = cv2.QT_FONT_NORMAL
-                id = 0
-                id += 1
-                name = labels[id_]
-                current_name = name
-                color = (255, 0, 0)
-                stroke = 2
-                cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
-
-                select = "SELECT student_id, name, DAY(login_date), MONTH(login_date), YEAR(login_date) FROM Student WHERE name='%s'" % (name)
-                name = cursor.execute(select)
-                result = cursor.fetchall()
-                data = "error"
-
-                for x in result:
-                    data = x
-
-                if data == "error":
-                    print("The student", current_name, "is NOT FOUND in the database.")
-                else:
-                    # Update the data in the database
-                    update = "UPDATE Student SET login_date=%s WHERE name=%s"
-                    val = (date, current_name)
-                    cursor.execute(update, val)
-                    update = "UPDATE Student SET login_time=%s WHERE name=%s"
-                    val = (current_time, current_name)
-                    cursor.execute(update, val)
-                    myconn.commit()
-
-                    hello = ("Hello ", current_name, "You did attendance today")
-                    print(hello)
-                    engine.say(hello)
-                    # engine.runAndWait()
-
-            else:
-                color = (255, 0, 0)
-                stroke = 2
-                font = cv2.QT_FONT_NORMAL
-                cv2.putText(frame, "UNKNOWN", (x, y), font, 1, color, stroke, cv2.LINE_AA)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
-                hello = ("Your face is not recognized")
-                print(hello)
-                engine.say(hello)
-                # engine.runAndWait()
-
-        cv2.imshow('Attendance System', frame)
-        k = cv2.waitKey(20) & 0xff
-        if k == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
 
 def login():
     user_input = login_widget.e_id.text()
@@ -137,7 +39,9 @@ def login():
         user = database.getStudent(user_input)
         if user is not None:
             logedin_user = user
-            recognize_faces()
+            with open("FaceRecognition/faces.py", 'r') as file:
+                code = file.read()
+                exec(code)
             main_stack.setCurrentIndex(1)
         else:
             QMessageBox.critical(login_widget, "Oops", "User not found in database.")
