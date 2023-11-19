87d471b2f3ebf951acddd9ab73aa16d94d77c574
amariszhangg <115908439+amariszhangg@users.noreply.github.com>
2023-11-19 11:57:52 +0800

    adding a function check_class that doesn't work currently

--- home.py
+++ home.py
@@ -16,3 +16,19 @@ home_layout.setAlignment(Qt.AlignCenter)
 home_layout.addWidget(QLabel("This is home page"))
 
 home_widget.setLayout(home_layout)
+
+
+student_id = 2  # change to imported student id
+def check_class():
+    today = datetime.now().date()  # today's date
+    time = datetime.now().time()  # current time
+    cursor.execute(  # this sql query does not work
+        f"SELECT * FROM Schedule WHERE DATE(start_time)='{today}' AND '{time}' <= TIME(start_time) NATURAL JOIN SELECT course_code FROM Enrolled WHERE student_id='{student_id}' AS E ORDER BY start_time LIMIT 1")
+    earliest = cursor.fetchone()
+    print(earliest)
+    time_diff = earliest[1] - datetime.now()  # get time difference from the next earliest class
+    if time_diff.total_seconds()//60 <= 60:
+        print("You have class in the next hour")
+        return True
+    print("No class in the next hour")
+    return False
