541c432ab603fac708e5222927212eb8d8f69292
amariszhangg <115908439+amariszhangg@users.noreply.github.com>
2023-11-19 12:07:51 +0800

    Update home.py

--- home.py
+++ home.py
@@ -32,13 +32,13 @@ student_id = 2  # change to imported student id
 def check_class():
     today = datetime.now().date()  # today's date
     time = datetime.now().time()  # current time
-    cursor.execute(  # this sql query does not work
-        f"SELECT * FROM Schedule WHERE DATE(start_time)='{today}' AND '{time}' <= TIME(start_time) NATURAL JOIN SELECT course_code FROM Enrolled WHERE student_id='{student_id}' AS E ORDER BY start_time LIMIT 1")
-    earliest = cursor.fetchone()
-    print(earliest)
-    time_diff = earliest[1] - datetime.now()  # get time difference from the next earliest class
-    if time_diff.total_seconds()//60 <= 60:
-        print("You have class in the next hour")
-        return True
+    cursor.execute(
+        f"SELECT * FROM (SELECT * FROM Schedule WHERE DATE(start_time)='{today}' AND '{time}' <= TIME(start_time)) as S NATURAL JOIN (SELECT course_code FROM Enrolled WHERE student_id='{student_id}') AS E ORDER BY start_time LIMIT 1")
+    earliest = cursor.fetchone()  # fetch next earliest class
+    if earliest:  # if there is a class
+        time_diff = earliest[1] - datetime.now()  # get their time difference
+        if time_diff.total_seconds()//60 <= 60:  # if it is within an hour
+            print("You have class in the next hour")
+            return True
     print("No class in the next hour")
     return False
