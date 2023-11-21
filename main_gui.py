547761714d315b972167481cadeaf98c7fa08f21
amariszhangg <115908439+amariszhangg@users.noreply.github.com>
2023-11-21 22:44:07 +0800

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,12 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QFrame, QWidget, QMainWindow, QMessageBox
-from PyQt5.QtWidgets import QStackedWidget
-from PyQt5.QtWidgets import QWidget, QStackedWidget
-from PyQt5.QtWidgets import QToolButton
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
-from PyQt5.QtCore import Qt, QSize
-
+from PyQt5.QtWidgets import QWidget, QStackedWidget, QToolButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import Qt
 from home import home_widget
 from schedule import schedule_widget
 
