bbb012f8abd16595bec8646139bfc4979b623a88
Dex-Wong <107986590+Dex-Wong@users.noreply.github.com>
2023-11-23 01:02:38 +0800

    Updated font size & positioning of home.py

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QFont, QPixmap, QPalette, QBrush
 from PyQt5.QtCore import Qt
 from datetime import datetime
 import database
@@ -8,48 +8,54 @@ import data
 home_widget = QWidget()
 
 home_layout = QVBoxLayout()
-home_layout.setAlignment(Qt.AlignCenter)
+home_layout.setAlignment(Qt.AlignTop)
+home_layout.setContentsMargins(20, 20, 20, 20)
+home_layout.setSpacing(20)
 
 home_widget.setLayout(home_layout)
 
-label = QLabel("abc")
-home_layout.addWidget(label)
+login = QLabel()
+stay = QLabel()
+login.setAlignment(Qt.AlignLeft)
+login.setFont(QFont('Arial', 10))
+stay.setAlignment(Qt.AlignLeft)
+stay.setFont(QFont('Arial', 10))
+
+label = QLabel()
+label.setAlignment(Qt.AlignCenter)
+label.setFont(QFont('Arial', 25)) 
 
 welcome = QLabel()
-home_layout.addWidget(welcome)
 welcome.setAlignment(Qt.AlignCenter)
 welcome.setFont(QFont('Arial', 70))
 
-login = QLabel()
-home_layout.addWidget(login)
-
-stay = QLabel()
-home_layout.addWidget(stay)
-
+home_layout.addWidget(login, alignment=Qt.AlignTop | Qt.AlignLeft)  
+home_layout.addWidget(stay, alignment=Qt.AlignTop | Qt.AlignLeft) 
+home_layout.addWidget(welcome)
+home_layout.addWidget(label)
 
 # check if you have class
-def check_class():  
+def check_class():
     lecture = database.getUpcomingClass(data.student_id)
     if lecture:
         course_code = lecture[0]
         start_time = lecture[1]
         time_diff = start_time - datetime.now()
         if time_diff.total_seconds() // 60 <= 60:
-            return f"You have {course_code} in the next hour at {start_time.time()}"  + \
-                f"Please see the relevant course materials."
-    
-    return "No class in the next hour."
+            return f"You have {course_code} in the next hour at {start_time.time()}. " \
+                   f"Please see the relevant course materials."
 
+    return "No class in the next hour."
 
 # update homepage
 def update_home_content():
-    label.setText(check_class())  
-    #show name 
+    label.setText(check_class())
+    # show name
     student_name = database.getStudent(data.student_id)[1]
     welcome.setText(f"Hello {student_name}")
-    #show last login time
+    # show last login time
     last_login = database.LoginTime(data.student_id)
     login.setText(f"Last login: {last_login}")
-    #show how long does the user stay
+    # show how long the user has stayed
     staytime = database.StayTime(data.student_id)
     stay.setText(f"Stay Time: {staytime}")
